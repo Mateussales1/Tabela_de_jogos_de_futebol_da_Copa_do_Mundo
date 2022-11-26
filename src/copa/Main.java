000b55af5a0ce4732d649d25e6d313c18f3fba70
Mateussales1 <s.mateussales@gmail.com>
2022-11-25 22:19:09 -0300

    cleaning code

--- src/copa/Main.java
+++ src/copa/Main.java
@@ -7,50 +7,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main (String [] args){
-    Scanner leitura;
-    leitura = new Scanner(System.in);
+
+    Selecao selecao1 = new Selecao();
+
+    selecao1.setNome("Brasilian");
+    selecao1.setCodSelecao(01);
+    System.out.println(selecao1.getNome());
 
 
-    ArrayList<Selecao> selecoes = new ArrayList<Selecao>();
-    ArrayList<Copa> copas = new ArrayList<Copa>();
-
-
-        Copa dadosCopa = new Copa();
-
-        Selecao dados = new Selecao();
-
-        System.out.println("Digite o nome da Seleção");
-            dados.setNome(leitura.next());
-
-        System.out.println("Digite o numero de jogos");
-        dados.setJogos(leitura.nextInt());
-
-        System.out.println("Digite o numero de vitorias");
-        dados.setVitorias(leitura.nextInt());
-
-        System.out.println("Digite o numero de empates");
-        dados.setEmpates(leitura.nextInt());
-
-        System.out.println("Digite o numero de derrotas");
-        dados.setDerrotas(leitura.nextInt());
-
-        System.out.println("Digite o numero de gols a favor");
-        dados.setGolFavor(leitura.nextInt());
-
-        System.out.println("Digite o numero de gols contra");
-        dados.setGolContra(leitura.nextInt());
-
-        //adiciona no arrayList
-        selecoes.add(dados);
-
-        System.out.println("deseja continuar cadastrando seleções para essa copa?");
-
-
-        dadosCopa.setSelecoes(selecoes);
-        copas.add(dadosCopa);
-
-        System.out.println("cadastrar outra copa?");
-
-
-}
+    }
 }
--- src/copa/Selecao.java
+++ src/copa/Selecao.java
@@ -2,64 +2,99 @@ package copa;
 
 
 public class Selecao {
-   private  String nome;
-   private int vitorias, jogos, empates, derrotas, golFavor, golContra,
-           saldoDeGols, codSeleção;
+    private String nome;
+    private int vitorias, jogos, empates, derrotas, golFavor, golContra,
+            saldoDeGols, codSelecao;
 
-   private double aproveitamento;
+    private double aproveitamento;
 
-        public String getNome() {
-            return nome;
-        }
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-        public int getVitorias() {
-            return vitorias;
-        }
-        public void setVitorias(int vitorias) {
-            this.vitorias = vitorias;
-        }
-        public int getJogos() {
-            return jogos;
-        }
-        public void setJogos(int jogos) {
-            this.jogos = jogos;
-        }
-        public int getEmpates() {
-            return empates;
-        }
-        public void setEmpates(int empates) {
-            this.empates = empates;
-        }
-        public int getDerrotas() {
-            return derrotas;
-        }
-        public void setDerrotas(int derrotas) {
-            this.derrotas = derrotas;
-        }
-        public int getGolFavor() {
-            return golFavor;
-        }
-        public void setGolFavor(int golFavor) {
-            this.golFavor = golFavor;
-        }
-        public int getGolContra() {
-            return golContra;
-        }
-        public void setGolContra(int golContra) {
-            this.golContra = golContra;
-        }
-        public int getSaldoDeGols() {
-            return saldoDeGols;
-        }
-        public void setSaldoDeGols(int saldoDeGols) {
-            this.saldoDeGols = saldoDeGols;
-        }
-        public int getCodSeleção() {
-            return codSeleção;
-        }
-        public void setCodSeleção(int saldoDeGols) {
-            this.codSeleção = codSeleção;
+    public String getNome() {
+        return nome;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(int jogos) {
+        this.jogos = jogos;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getGolFavor() {
+        return golFavor;
+    }
+
+    public void setGolFavor(int golFavor) {
+        this.golFavor = golFavor;
+    }
+
+    public int getGolContra() {
+        return golContra;
+    }
+
+    public void setGolContra(int golContra) {
+        this.golContra = golContra;
+    }
+
+    public int getSaldoDeGols() {
+        return saldoDeGols;
+    }
+
+    public void setSaldoDeGols(int saldoDeGols) {
+        this.saldoDeGols = saldoDeGols;
+    }
+
+    public int getCodSelecao() {
+        return codSelecao;
+    }
+
+    public void setCodSelecao(int saldoDeGols) {
+        this.codSelecao = codSelecao;
+    }
+
+    public void setAproveitamento() {
+        this.aproveitamento = aproveitamento;
+
+    }
+
+    public double getAproveitamento(){
+        return aproveitamento;
+    }
+
+    public double calculoAproveitamento(int vitorias, int derrotas, int empates){
+        int total = vitorias + derrotas + empates;
+
+        return ((vitorias / total) * 100);
+    }
+
 }
+
+
