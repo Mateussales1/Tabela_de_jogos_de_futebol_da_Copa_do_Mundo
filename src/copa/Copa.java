9fa4a951a4943fe6139d1b6c16814f0d8ab086ca
Mateussales1 <s.mateussales@gmail.com>
2022-11-25 21:27:06 -0300

    first commit

---
+++ src/copa/Copa.java
@@ -0,0 +1,14 @@
+package copa;
+
+import java.util.ArrayList;
+
+public class Copa {
+    private ArrayList<Selecao> selecoes;
+
+    public ArrayList<Selecao> getSelecoes() {
+        return selecoes;
+    }
+    public void setSelecoes(ArrayList<Selecao> selecoes) {
+        this.selecoes = selecoes;
+    }
+}
